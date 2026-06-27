f3fc33bd36406a821f32e43122998bafd66ee3fe
ayush <109770319+ayushcodes1148@users.noreply.github.com>
2026-06-27 00:12:59 -0400

    Add filtering methods for products in FilterSearch

--- FilterSearch.java
+++ FilterSearch.java
@@ -1,21 +1,58 @@
 import java.util.ArrayList;
-// ProductTags are delimited with a comma.
-// Products are of the format productName|productPrice|productType|productBrand|productTags|productDescription
+import java.util.List;
 
 public class FilterSearch {
-    private String category;
-    private String type;
-
-    public FilterSearch(String category, String type) {
-        this.category = category;
-        this.type = type;
+    public List<Product> filterPriceLow(List<Product> searchResult) {
+        List<Product> filteredResult = new ArrayList<>(searchResult);
+        for (int index = 0; index < filteredResult.size() - 1; index++) {
+            for (int index2 = 0; index2 < filteredResult.size() - index - 1; index2++) {
+                Product firstProduct = filteredResult.get(index2 + 1);
+                Product secondProduct = filteredResult.get(index2);
+                if (firstProduct.getPrice() < secondProduct.getPrice()) {
+                    Product saved = firstProduct;
+                    filteredResult.set(index2 + 1, secondProduct);
+                    filteredResult.set(index2, saved);
+                }
+            }
+        }
+        return filteredResult;
     }
 
-    public String getCategory() {
-        return category;
+    public List<Product> filterPriceHigh(List<Product> searchResult) {
+        List<Product> filteredResult = new ArrayList<>(searchResult);
+        for (int index = 0; index < filteredResult.size() - 1; index++) {
+            for (int index2 = 0; index2 < filteredResult.size() - index - 1; index2++) {
+                Product firstProduct = filteredResult.get(index2 + 1);
+                Product secondProduct = filteredResult.get(index2);
+                if (firstProduct.getPrice() > secondProduct.getPrice()) {
+                    Product saved = firstProduct;
+                    filteredResult.set(index2 + 1, secondProduct);
+                    filteredResult.set(index2, saved);
+                }
+            }
+        }
+        return filteredResult;
     }
 
-    public String getType() {
-        return type;
+    public List<Product> filterStock(List<Product> searchResult) {
+        List<Product> filteredResult = new ArrayList<>();
+        for (int index = 0; index < searchResult.size(); index++) {
+            if (searchResult.get(index).getStock() > 0) {
+                filteredResult.add(searchResult.get(index));
+            }
+        }
+        return filteredResult;
+    }
+
+    public List<Product> filterLocation(List<Product> searchResult, String location) {
+        List<Product> filteredResult = new ArrayList<>();
+        for (int index = 0; index < searchResult.size(); index++) {
+            String lowercaseLocation = searchResult.get(index).getLocation().toLowerCase();
+            String lowercaseUserLocation = location.toLowerCase();
+            if (lowercaseLocation.equals(lowercaseUserLocation.trim())) {
+                filteredResult.add(searchResult.get(index));
+            }
+        }
+        return filteredResult;
     }
 }
