3977a79f60a306038ae8a2b9b09ce27f7f1efbd5
LastCali <sentyblcbrry@gmail.com>
2026-06-28 13:44:24 -0400

    Minor fix.

--- SystemLogin.java
+++ SystemLogin.java
@@ -22,6 +22,7 @@ public class SystemLogin {
                 
                 if(selection == 1) {
                     login();
+                    break;
                 }
                 else if(selection == 2) {
                     CreateAccount();
@@ -36,10 +37,10 @@ public class SystemLogin {
     }
 
     public void login() {
-        System.out.print("Enter your username: "); 
+        System.out.print("Enter username: "); 
         String username = scanner.nextLine();
 
-        System.out.println("Enter your password: ");
+        System.out.print("Enter password: ");
         String password = scanner.nextLine();
 
         System.out.println("Enter 1 if you are a customer.");
@@ -62,7 +63,7 @@ public class SystemLogin {
 
         boolean verified = pManager.userVerification(username, password, category);
         if(verified == true) {
-            System.out.printf("Login successful, welcome %s", username);
+            System.out.printf("Login successful, welcome %s\n", username);
         }
         else {
             System.out.println("Login unsuccessful."); 
