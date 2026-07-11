55cd7685c332aa61988aed3bfb49b4f71cdba5cc
Adil Sami <adilsami009@gmail.com>
2026-07-10 21:25:48 -0400

    Create server files and add project dependency

---
+++ Server.java
@@ -0,0 +1,16 @@
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        
+        server.createContext("/api/products", new ProductHandler());
+        server.createContext("/api/login", new LoginHandler());
+        // server.createContext("/api/signup", new SignUpHandler());
+        
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server running on port 8080");
+    }
+}
