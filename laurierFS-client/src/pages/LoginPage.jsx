8371e31ae15699c2123cd57f34438616c4047aae
Adil Sami <adilsami009@gmail.com>
2026-07-23 22:14:59 -0400

    add login/signup toggle

--- laurierFS-client/src/pages/LoginPage.jsx
+++ laurierFS-client/src/pages/LoginPage.jsx
@@ -8,32 +8,84 @@ import "../styles/LoginPage.css"
 import { login } from "../services/authService"
 
 const LoginPage = ()=> {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
     const [showpassword, setShowPassword] = useState(false)
+    const [isSignUp, setIsSignUp] = useState(false)
+    const [formData, setFormData] = useState({
+        identifier: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
     const navigate = useNavigate()
 
-    const handleLogin = () => {
+    const handleChange = (e) =>{
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = (e) => {
+        if (e) e.preventDefault();
+
         const result = login(email, password)
         result.role==="admin"?navigate("/admin"):navigate("/search")
+    }
+    const toggleAuth = (e) =>{
+        e.preventDefault()
+        setIsSignUp(!isSignUp)
+        setFormData({
+            identifier: '',
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        })
     }
 
     return(
     <div className="login-page">
         <div className="login-card">
             <Logo></Logo>
-            <InputField type="text" placeholder="Username or Email" onChange={(e)=> setEmail(e.target.value)} value={email}></InputField>
-            <div className="password-wrapper">
-                <InputField type={showpassword ? "text" : "password"} placeholder="Password" onChange={(e)=> setPassword(e.target.value)} value={password}></InputField>
+            <form onSubmit={handleSubmit}>
+                {/* Username + email if signing up OR Username/Email if logging in */}
+                {isSignUp? 
+                    (<>
+                        <InputField name="username" type="text" placeholder="Username" onChange={handleChange} value={formData.username}/>
+                        <InputField name="email" type="email" placeholder="Email" onChange={handleChange} value={formData.email}/>
+                    </>)
+                :   (<>
+                        <InputField name="identifier" type="text" placeholder="Username or Email" onChange={handleChange} value={formData.identifier}/>
+                    </>)
+                }
+
+                {/* Password field */}
+                <div className="password-wrapper">
+                    <InputField name="password" type={showpassword ? "text" : "password"} placeholder="Password" onChange={handleChange} value={formData.password}/>
+                    <button className="show-btn" onClick={()=>setShowPassword(!showpassword)} onMouseDown={(e)=>e.preventDefault()} type="button">{showpassword?<FaEyeSlash size={18}/>:<FaEye size={18}/>}</button>
+                </div>
+
+                {/* Confirm password (if signup) */}
+                {isSignUp && 
+                    <div className="password-wrapper">
+                        <InputField name="confirmPassword" type={showpassword ? "text" : "password"} placeholder="Confirm Password" onChange={handleChange} value={formData.confirmPassword}/>
+                        <button className="show-btn" onClick={()=>setShowPassword(!showpassword)} onMouseDown={(e)=>e.preventDefault()} type="button">{showpassword?<FaEyeSlash size={18}/>:<FaEye size={18}/>}</button>
+                    </div>
+                }
+
+                {/* Submit button */}
+                <Button className="login-btn" onClick={handleSubmit} label={isSignUp?"Sign Up":"Login"} type="submit"></Button>
+
+            </form>                
                 
-                <button className="show-btn" onClick={()=>setShowPassword(!showpassword)} onMouseDown={(e)=>e.preventDefault()} type="button">{showpassword?<FaEyeSlash size={18}/>:<FaEye size={18}/>}</button>
-                
+            
+            <div>
+                {isSignUp?"Already have an account? ":"Don't have an account? "} 
+                <a href="#" onClick={toggleAuth}>{isSignUp?"Login":"Sign Up"}</a>
             </div>
-            <Button className="login-btn" onClick={handleLogin} label="Login" type="button"></Button>
-            <div>OR</div>
-            <Button className="login-btn" onClick={()=>{}} label="Sign Up" type="button"></Button>
-            <a href="#">Forgot Password?</a>
+            {!isSignUp && <a href="#">Forgot Password?</a>}
             <a href="#">Continue as Guest</a>
+            {/* <a href="#" onClick={(e) => { e.preventDefault(); navigate("/distributor-login") }}>Distributor Login Page</a> */}
         </div>
     </div>
 )}
