4968a0096c819de5def3bb1c9eb6c8e8bb3148db
Dipo006 <joladipo717@gmail.com>
2026-07-25 19:29:21 -0400

    Update App.jsx

--- laurierFS-client/src/App.jsx
+++ laurierFS-client/src/App.jsx
@@ -8,6 +8,7 @@ import DistributorDashboardPage from './pages/DistributorDashboardPage';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { CartProvider } from "./context/CartContext"
 import { OrderProvider } from "./context/OrderContext"
+import ProductReturnsPage from "./pages/ProductReturnsPage";
 
 function App() {
   return (
